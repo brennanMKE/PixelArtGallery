de80d30921d73e4fea4a8149149e2ee440fbcc8f
Brennan Stehling <277419+brennanMKE@users.noreply.github.com>
2026-04-06 16:24:31 -0700

    init

---
+++ PixelArtGallery/PixelArtGalleryApp.swift
@@ -0,0 +1,12 @@
+// PixelArtGalleryApp.swift
+
+import SwiftUI
+
+@main
+struct PixelArtGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
